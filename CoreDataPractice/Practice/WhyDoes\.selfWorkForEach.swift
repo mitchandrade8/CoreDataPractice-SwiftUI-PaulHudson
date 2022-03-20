fa150f932138fd904e352a00c454bef1313d1105
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-20 12:25:46 -0600

    first commit

---
+++ CoreDataPractice/Practice/WhyDoes\.selfWorkForEach.swift
@@ -0,0 +1,20 @@
+//
+//  WhyDoes\.selfWorkForEach.swift
+//  CoreDataPractice
+//
+//  Created by Mitch Andrade on 3/20/22.
+//
+
+import SwiftUI
+
+struct WhyDoes__selfWorkForEach: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WhyDoes__selfWorkForEach_Previews: PreviewProvider {
+    static var previews: some View {
+        WhyDoes__selfWorkForEach()
+    }
+}
