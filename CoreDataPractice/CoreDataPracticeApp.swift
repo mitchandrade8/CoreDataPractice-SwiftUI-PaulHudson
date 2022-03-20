e71fbc207917b3b7687511b33bfb4cb66e4326d5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-20 12:01:31 -0600

    Initial Commit

---
+++ CoreDataPractice/CoreDataPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPracticeApp.swift
+//  CoreDataPractice
+//
+//  Created by Mitch Andrade on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
